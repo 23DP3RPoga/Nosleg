360d9f8ad438b06f91b242356b225bb6f0ac6a66
Ralfs Rodrigo Poga <166013257+23DP3RPoga@users.noreply.github.com>
2026-05-18 20:55:40 +0000

    PLS

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -25,3 +25,31 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+<?php
+
+use App\Http\Middleware\CorsMiddleware;
+use App\Http\Middleware\EnsureUserIsAdmin;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(CorsMiddleware::class); // ← add this line
+        $middleware->alias([
+            'admin' => EnsureUserIsAdmin::class,
+        ]);
+        $middleware->api(remove: [
+            EnsureFrontendRequestsAreStateful::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
